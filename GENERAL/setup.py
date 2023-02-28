5adb136958e3e0a080f0d28929cb908a400b8e85
fedluch <luchellifederico@gmail.com>
2023-02-28 14:32:40 -0300

    subiendo preEntrega2

---
+++ GENERAL/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    
+    name="segunda_preEntrega_Luchelli",
+    version="1.0",
+    description="Segunda preEntrega",
+    author="Federico Luchelli",
+    author_email="luchellifederico@gmail.com",
+    
+    packages=["paquete"]
+)
